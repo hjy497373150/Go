f979d5cf6bb567c546be862be6e5c530db6ffec1
klayhu <klayhu@tencent.com>
2023-01-10 20:22:45 +0800

    增加用户消息广播功能，可以发消息告诉其他人了

--- studyGolang/IM_System/server.go
+++ studyGolang/IM_System/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -70,6 +71,26 @@ func (this *MyServer)Handler(conn net.Conn) {
 	// 广播告知所有在线用户 有人上线了
 	this.BoardCast(user,"已上线")
 
+	// 接收客户端发送的消息并广播，核心是启动一个针对客户端conn的读gorountine
+	go func() {
+		buf := make([]byte,4096) // 4K大小，超过会有问题
+		for {
+			n, err := conn.Read(buf) //把信息读在buf中
+			if n == 0 {
+				this.BoardCast(user,"下线")
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("read conn err:", err)
+				return
+			}
+
+			// 提取用户的信息，去除\n
+			msg := string(buf[:n-1])
+
+			// 将得到的消息进行广播
+			this.BoardCast(user,msg)
+		}
+	}()
 }
 
 // 广播消息的方法
