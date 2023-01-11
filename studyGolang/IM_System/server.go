65f3406c0f25924da2b2c176673219698f1c80e0
klayhu <klayhu@tencent.com>
2023-01-11 11:39:08 +0800

    用户业务封装，包括上线下线和消息的处理

--- studyGolang/IM_System/server.go
+++ studyGolang/IM_System/server.go
@@ -61,15 +61,10 @@ func (this *MyServer)Handler(conn net.Conn) {
 	// fmt.Println("连接建立成功...")
 
 	// 创建用户
-	user := NewUser(conn)
+	user := NewUser(conn, this)
 
-	// 用户上线 将用户加到onlineMapUser中
-	this.mapLock.Lock()
-	this.onlineMapUser[user.Name] = user
-	this.mapLock.Unlock()
-
-	// 广播告知所有在线用户 有人上线了
-	this.BoardCast(user,"已上线")
+	// 用户上线
+	user.Online()
 
 	// 接收客户端发送的消息并广播，核心是启动一个针对客户端conn的读gorountine
 	go func() {
@@ -77,7 +72,9 @@ func (this *MyServer)Handler(conn net.Conn) {
 		for {
 			n, err := conn.Read(buf) //把信息读在buf中
 			if n == 0 {
-				this.BoardCast(user,"下线")
+				// 用户下线
+				user.Offline()
+				return
 			}
 			if err != nil && err != io.EOF {
 				fmt.Println("read conn err:", err)
@@ -87,8 +84,8 @@ func (this *MyServer)Handler(conn net.Conn) {
 			// 提取用户的信息，去除\n
 			msg := string(buf[:n-1])
 
-			// 将得到的消息进行广播
-			this.BoardCast(user,msg)
+			// 用户针对msg进行处理
+			user.DoMessage(msg)
 		}
 	}()
 }
--- studyGolang/IM_System/user.go
+++ studyGolang/IM_System/user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C chan string
 	Conn net.Conn // 用户信息的读写
+
+	Server *MyServer
 }
 
 // 创建一个用户的接口
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *MyServer) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,6 +20,7 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C : make(chan string),
 		Conn: conn,
+		Server: server,
 	}
 	// 启动监听当前user channel消息的goroutine
 
@@ -33,4 +36,29 @@ func (this *User) listenMessage() {
 
 		this.Conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// 封装上线功能
+func (this *User)Online() {
+	// 用户上线 将用户加到onlineMapUser中
+	this.Server.mapLock.Lock()
+	this.Server.onlineMapUser[this.Name] = this
+	this.Server.mapLock.Unlock()
+
+	// 广播告知所有在线用户 有人上线了
+	this.Server.BoardCast(this,"已上线")
+} 
+
+// 下线
+func (this *User)Offline() {
+	this.Server.mapLock.Lock()
+	delete(this.Server.onlineMapUser, this.Name) //将用户从map中删除
+	this.Server.mapLock.Unlock()
+
+	this.Server.BoardCast(this,"下线")
+}
+
+// 用户处理消息的业务
+func (this *User)DoMessage(msg string) {
+	this.Server.BoardCast(this, msg)
 }
